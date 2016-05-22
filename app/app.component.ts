52b73ce74fe9415e958ddb5133912b5b0dd07598
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-05-22 13:31:36 -0700

    feat: add app config.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, provide } from '@angular/core';
+import { Component, Inject, provide } from '@angular/core';
 
+import { APP_CONFIG, CONFIG } from './app.config';
 import { BetterLogger } from './better-logger.service';
 import { HeroesComponent } from './heroes/heroes.component';
 import { Logger } from './logger.service';
@@ -8,12 +9,22 @@ import { UserService } from './user.service';
 @Component({
     selector: 'my-app',
     template: `
+      <h1>{{ title }}</h1>
       <my-heroes></my-heroes>
     `,
     directives: [HeroesComponent],
     providers: [
-      UserService,
+      UserService, // must be before APP_CONFIG
+
+      provide(APP_CONFIG, { useValue: CONFIG }),
       provide(Logger, { useClass: BetterLogger }
     ]
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(
+    @Inject(APP_CONFIG) config:Config,
+    private userService:UserService) {
+
+    this.title = config.title;
+  }
+}
